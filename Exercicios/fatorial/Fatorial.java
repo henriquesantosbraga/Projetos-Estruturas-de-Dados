e9f2baee23ff64c1be142b61c2edcaf2d1977ee8
Henrique S. Braga <henriquesantosbraga123@gmail.com>
2026-03-16 17:05:45 -0300

    Add files via upload

---
+++ Exercicios/fatorial/Fatorial.java
@@ -0,0 +1,9 @@
+import Exercicios.fatorial.FatorialController;
+
+public class Fatorial {
+    public static void main(String[] args) {
+		FatorialController imp = new FatorialController();
+		
+		System.out.println(imp.Calcular(6));
+	}
+}
---
+++ Exercicios/fatorial/FatorialController.java
@@ -0,0 +1,11 @@
+public class FatorialController {
+	public FatorialController() {
+		super();
+	}
+	public int calcular(int numero) {
+		if(numero <= 1) {
+			return 1;
+		}
+		return numero * calcular (--numero);
+	}
+}
